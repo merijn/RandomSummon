6319a5da28dcbf7322b47c5001676d7ece41e0d0
Merijn Verstraaten <merijn@inconsistent.nl>
2022-09-30 01:43:08 +0200

    Be more selective about when we care about falling.

--- RandomSummon.lua
+++ RandomSummon.lua
@@ -115,8 +115,6 @@ local function CheckBusy()
         return "CASTING"
     elseif UnitChannelInfo("player") then
         return "CHANNELING"
-    elseif IsFalling() then
-        return "FALLING"
     end
 
     local start, duration, enabled, modRate = GetSpellCooldown(61304)
@@ -298,7 +296,11 @@ local function RandomSummon_OnEvent(self, event, ...)
         end
     elseif event == "PLAYER_MOUNT_DISPLAY_CHANGED" then
         if not IsMounted() then
-            C_Timer.After(0.12, function() EnsureRandomCompanion() end)
+            C_Timer.After(0.12, function()
+                if not IsFalling() then
+                    EnsureRandomCompanion()
+                end
+            end)
         end
     elseif event == "UPDATE_SHAPESHIFT_FORM" then
         print(event, ...)
